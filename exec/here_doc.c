6834cef3b295ccb659864d4e293a0f8dc31f4397
Hamza El Ouardi <hel-ouar@z3r13p1.42lyon.fr>
2023-04-17 13:16:50 +0200

    free line dans heredoc

--- exec/here_doc.c
+++ exec/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: hel-ouar <hel-ouar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 15:55:53 by ikaismou          #+#    #+#             */
-/*   Updated: 2023/04/17 12:22:11 by hel-ouar         ###   ########.fr       */
+/*   Updated: 2023/04/17 13:14:49 by hel-ouar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,16 @@ void	remove_heredoc(char **tab)
 
 int	here_doc(t_minishell *ms, char *tab)
 {
-	char	*tab2 = ft_strjoin(".", tab);
+	int		dollar;
+	char	*tab2;
+	char	*tmp;
+	
+	tab2 = ft_strjoin(".", tab);
 	ms->infile = open(tab2, O_CREAT | O_RDWR | O_TRUNC, 0644);
 	while (1)
 	{
 		//g_exit_status = 1;
+		dollar = 0;
 		ms->line_here = readline(">");
 		if (!ms->line_here)
 			return (free(ms->line_here), 0);
@@ -43,10 +48,16 @@ int	here_doc(t_minishell *ms, char *tab)
 				&& (ft_strlen(tab) == ft_strlen(ms->line_here)))
 			break ;
 		if (ft_strnstr(ms->line_here, "$", ft_strlen(ms->line_here)) != 0)
+		{
+			dollar = 1;
+			tmp = ms->line_here;
 			ms->line_here = ft_strdup(dollar_exist(ms, ms->line_here, 0, 0));
+			free(tmp);
+		}
 		write(ms->infile, ms->line_here, ft_strlen(ms->line_here));
 		write(ms->infile, "\n", 1);
-		//free(ms->line_here);
+		if (!dollar)
+			free(ms->line_here);
 	}
 	free(ms->line_here);
 	close(ms->infile);
