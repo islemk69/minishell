e54bf0569d14fcee25e79ef9cef1cef1358829b1
Islem Kaismoun <ikaismou@ikaismou>
2023-04-28 16:38:15 +0200

    asdsd

--- exec/simple_exec.c
+++ exec/simple_exec.c
@@ -6,7 +6,7 @@
 /*   By: ikaismou <ikaismou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 15:55:06 by ikaismou          #+#    #+#             */
-/*   Updated: 2023/04/28 14:49:44 by ikaismou         ###   ########.fr       */
+/*   Updated: 2023/04/28 16:37:15 by ikaismou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,6 @@ int	exec_one_pipe(t_minishell *ms, t_env **env)
 
 	int i = 0;
 	get_path(ms);
-	if (builtins(ms, ms->parsed, env, 0))
-			return (1);
 	// while (ms->parsed[i] && ms->parsed[i][0] == '<')
 	// {
 	// 	if (ms->parsed[i][1] == '<')
@@ -69,11 +67,13 @@ int	exec_one_pipe(t_minishell *ms, t_env **env)
 			rm_quote_last(ms->parsed);
 			ms->new_parsed = ms->parsed;
 		}
+		if (builtins(ms, ms->new_parsed, env, 0))
+			exit(0);
 		if (!check_command(ms, ms->new_parsed[0]))
 			exit (0);
 		execve(ms->path_cmd, ms->new_parsed, refresh_env(env));
 	}
-	wait(&ms->status);
+	waitpid(1, &ms->status, 1);
 	g_global.g_status = WEXITSTATUS(ms->status);
 	remove_heredoc(ms->parsed);
 	return (1);
