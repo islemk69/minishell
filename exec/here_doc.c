2dc8cb630f30349c33eb683942001d636e751bed
Islem Kaismoun <ikaismou@ikaismou>
2023-05-05 19:08:43 +0200

    corr bug

--- exec/here_doc.c
+++ exec/here_doc.c
@@ -6,7 +6,7 @@
 /*   By: ikaismou <ikaismou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 15:55:53 by ikaismou          #+#    #+#             */
-/*   Updated: 2023/05/05 17:52:57 by ikaismou         ###   ########.fr       */
+/*   Updated: 2023/05/05 19:06:47 by ikaismou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,7 @@ int	here_doc(t_minishell *ms, char *tab, char *w_quote)
 	int		dollar;
 	char	*tab2;
 	char	*tmp;
+	char **norm;
 	
 	set_heredoc_signals();
 	tab2 = ft_strjoin(".", tab);
@@ -54,7 +55,8 @@ int	here_doc(t_minishell *ms, char *tab, char *w_quote)
 		{
 			dollar = 1;
 			tmp = ms->line_here;
-			//ms->line_here = ft_strdup(dollar_exist(ms, ms->line_here, 0, 0));
+			norm = dollar_exist(ms, ms->line_here, 0, 0);
+			ms->line_here = norm[0];
 			free(tmp);
 		}
 		write(ms->infile, ms->line_here, ft_strlen(ms->line_here));
