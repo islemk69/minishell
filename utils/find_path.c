026ab00eb377752c3e732f98b114e6f0d37f8444
Hamza <hamza.elouardi1@gmail.com>
2023-05-17 05:17:50 +0200

    norme find path de wa3ar hahaha

--- utils/find_path.c
+++ utils/find_path.c
@@ -6,23 +6,44 @@
 /*   By: hamza <hamza@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 23:52:48 by ikaismou          #+#    #+#             */
-/*   Updated: 2023/05/17 04:54:33 by hamza            ###   ########.fr       */
+/*   Updated: 2023/05/17 05:15:59 by hamza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+int	do_refresh_env(t_env **head, char ***new_env, int i)
+{
+	int	j;
+	int	k;
+
+	j = 0;
+	k = 0;
+	while ((*head)->key[k])
+		(*new_env)[i][j++] = (*head)->key[k++];
+	if (!(*head)->value)
+	{
+		(*head) = (*head)->next;
+		return (i);
+	}
+	(*new_env)[i][j] = '=';
+	j++;
+	k = 0;
+	while ((*head)->value[k])
+		(*new_env)[i][j++] = (*head)->value[k++];
+	(*new_env)[i][j] = 0;
+	(*head) = (*head)->next;
+	i++;
+	return (i);
+}
 
 char	**refresh_env(t_env **env)
 {
 	t_env	*head;
 	char	**new_env;
 	int		i;
-	int		j;
-	int		k;
 
 	i = 0;
-	j = 0;
-	k = 0;
 	head = *env;
 	new_env = (char **)ft_gc_malloc(sizeof(char *) * (lstsize(*env) + 1));
 	while (head)
@@ -33,23 +54,7 @@ char	**refresh_env(t_env **env)
 		else
 			new_env[i] = ft_gc_malloc(sizeof(char) * \
 				(ft_strlen(head->key) + ft_strlen(head->value) + 2));
-		j = 0;
-		k = 0;
-		while (head->key[k])
-			new_env[i][j++] = head->key[k++];
-		if (!head->value)
-		{
-			head = head->next;
-			continue ;
-		}
-		new_env[i][j] = '=';
-		j++;
-		k = 0;
-		while (head->value[k])
-			new_env[i][j++] = head->value[k++];
-		new_env[i][j] = 0;
-		head = head->next;
-		i++;
+		i = do_refresh_env(&head, &new_env, i);
 	}
 	return (new_env[i] = 0, new_env);
 }
